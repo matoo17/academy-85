7f96b34081ec135e90976e39d4f198bbe4eeffb6
Mato Mamuzić <matoo.mamuzic@gmail.com>
2022-05-08 13:56:38 +0200

    Zadatak 1

---
+++ String firstname='Vedran';.dart
@@ -0,0 +1,13 @@
+String firstname='Vedran';
+  String lastname='Balagovic';
+num age=30;
+num height=1.75;
+
+
+
+void main() {
+  print(firstname);
+    print(lastname);
+  print(age);
+  print(height);
+}
